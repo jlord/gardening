aabfe46ed8bae42e427d95c80299dcbfe165132b
Jessica Lord <jessica.lord@github.com>
2015-02-07 23:03:51 -0800

    i dunno maybe

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var exec = require('child_process').exec
 // I use a test account of mine, eviljlord,
 // it's very likely to have no activity
 
-test('check zero', function(t) {
+test('verify zero for today', function(t) {
   exec('node index.js eviljlord', function(error, stdout, stderr) {
     t.notOk(error, "no error running command")
     t.equal(stdout, "---\n✗ Grey! eviljlord with 0 today!\n---\n")
@@ -12,7 +12,7 @@ test('check zero', function(t) {
   t.end()
 })
 
-test('check zero yesterday', function(t) {
+test('verify zero for yesterday', function(t) {
   exec('node index.js eviljlord -y', function(error, stdout, stderr) {
     t.notOk(error, "no error running command")
     t.equal(stdout, "---\n✗ Grey! eviljlord with 0 yesterday!\n---\n")
